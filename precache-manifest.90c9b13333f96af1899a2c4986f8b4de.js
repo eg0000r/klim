8e5273e1f5453460e80f3ae434e3b47114b93d63
eg0000r <47872540+eg0000r@users.noreply.github.com>
2020-01-06 14:30:52 +0200

    Updates

---
+++ precache-manifest.90c9b13333f96af1899a2c4986f8b4de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a4c2eb90bdcf5dd2828db0942f43f",
+    "url": "/klim/index.html"
+  },
+  {
+    "revision": "aa8d2ad3eab7b27c4c5a",
+    "url": "/klim/static/css/main.47553e51.chunk.css"
+  },
+  {
+    "revision": "81adecfb92f211424706",
+    "url": "/klim/static/js/2.53fafbf3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/klim/static/js/2.53fafbf3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa8d2ad3eab7b27c4c5a",
+    "url": "/klim/static/js/main.dc118192.chunk.js"
+  },
+  {
+    "revision": "d04ac266c16bf87006ba",
+    "url": "/klim/static/js/runtime-main.1a0e4479.js"
+  }
+]);
